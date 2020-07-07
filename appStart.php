a2fef958ab6da4618d91923f15128ec80e329910
footballplus <62063221+footballplus@users.noreply.github.com>
2020-07-07 19:57:39 +0100

    Update appStart.php

--- appStart.php
+++ appStart.php
@@ -1,3 +1,3 @@
 {"deviceNo":"69520","deviceHash":"8a8780843d3bc5a4937588fdc1d7b8620c6d896b","updateMessage":"Uygulamay\u0131 kullanabilmek i\u00e7in g\u00fcncelleme yapman\u0131z gerekmektedir.","updatePackageName":"","updateServer":"https:\/\/play.google.com\/store\/apps\/details?id=com.mobiltvrehberim.app","updateStatus":false,"adsPeriod":"1","adsTimeout":"120","adMobBannerAds":true,"adMobInterstitialAds":true,
 
-"mediaServerUrl":"https://raw.githubusercontent.com/footballplus/com.hdlive.pro/3.0.0/3.0.0/getapi/medialist.txt","reportServerUrl":""}
+"mediaServerUrl":"https://github.com/footballplus/com.hdlive.pro/raw/3.0.0/3.0.0/getapi/medialist.txt","reportServerUrl":""}
