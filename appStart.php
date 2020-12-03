350cd8c538cf5abf2df5b0d31aaec4557a4f8d9a
footballplus <62063221+footballplus@users.noreply.github.com>
2020-12-03 13:08:09 +0100

    Update appStart.php

--- appStart.php
+++ appStart.php
@@ -1,3 +1,3 @@
 {"deviceNo":"69520","deviceHash":"8a8780843d3bc5a4937588fdc1d7b8620c6d896b","updateMessage":"Uygulamay\u0131 kullanabilmek i\u00e7in g\u00fcncelleme yapman\u0131z gerekmektedir.","updatePackageName":"","updateServer":"https:\/\/play.google.com\/store\/","updateStatus":false,"adsPeriod":"2","adsTimeout":"120","adMobBannerAds":false,"adMobInterstitialAds":false,
 
-"mediaServerUrl":"https://github.com/footballplus/com.hdlive.pro/raw/3.0.0/Liveapp","reportServerUrl":""}
+"mediaServerUrl":"https://github.com/footballplus/com.hdlive.pro/raw/3.0.0/Test","reportServerUrl":""}
